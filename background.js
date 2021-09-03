d3710f4b0b1c6cd7e7356a960602879e4e9973ea
terrysu64 <66130506+terrysu64@users.noreply.github.com>
2021-09-03 17:35:16 -0400

    Add files via upload

---
+++ background.js
@@ -0,0 +1,25 @@
+const defaultFilters = [
+	"*://*.doubleclick.net/*",
+	"*://partner.googleadservices.com/*",
+	"*://*.googlesyndication.com/*",
+	"*://*.google-analytics.com/*",
+	"*://creative.ak.fbcdn.net/*",
+	"*://*.adbrite.com/*",
+	"*://*.exponential.com/*",
+	"*://*.quantserve.com/*",
+	"*://*.scorecardresearch.com/*",
+	"*://*.zedo.com/*",
+	"*://*.bebi.com/*",
+	"*://*.moat.com/*",
+	"*://*.bunny.net/*",
+	"*://*.bidgear.com/*",
+	"*://*.thebestgame2020.com/*",
+	"*://*.chaturbate.com/*"
+]
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) { return { cancel: true }},
+    { urls: defaultFilters },
+    ["blocking"]
+)
+
